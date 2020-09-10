107939cf15b42c4376afc953119c39116e3ab166
Lane Coleman <lane.coleman@live.com>
2020-09-10 15:15:30 -0600

    Add project files.

---
+++ MultiplicationTableLC.vb
@@ -0,0 +1,28 @@
+﻿Module MultiplicationTableLC
+
+    Sub Main()
+        Dim numbers As String
+        Dim n, column, row As Integer
+        Dim data(12, 12) As Integer
+
+
+        'populating the array
+        Do
+            Console.WriteLine("Enter a number")
+            n = CInt(Console.ReadLine)
+
+            For column = 1 To n
+                For row = 1 To n
+                    data(row, column) = row * column
+                    Console.Write(data(row, column) & " ")
+                Next
+                Console.WriteLine()
+            Next
+            Console.ReadLine()
+            Console.Clear()
+        Loop
+    End Sub
+
+
+
+End Module
